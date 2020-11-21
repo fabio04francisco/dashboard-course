ba2837dc959dcf37c726691326a15c531d2169f5
Fabio Francisco <fabinhofrancisco@gmail.com>
2020-11-20 23:45:10 -0300

    feat: transforma image SVG em react components. Desta forma o precesso de estilização é mais facil.

---
+++ src/components/Icons/index.js
@@ -0,0 +1,21 @@
+import CourseIcon from './CourseIcon';
+import EmailIcon from './EmailIcon';
+import HomeIcon from './HomeIcon';
+import LogoCourse from './LogoCourseSVG';
+import LogoffIcon from './LogoffIcon';
+import SettingsIcon from './SettingsIcon';
+import UserIcon from './UserIcon';
+import LeftArrowIcon from './LeftArrow';
+import RightArrowIcon from './RightArrow';
+
+export {
+  LogoCourse,
+  CourseIcon as Course,
+  EmailIcon as Email,
+  HomeIcon as Home,
+  LogoffIcon as Logoff,
+  SettingsIcon as Settings,
+  UserIcon as User,
+  LeftArrowIcon as LeftArrow,
+  RightArrowIcon as RightArrow,
+};
