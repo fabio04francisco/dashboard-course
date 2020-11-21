bea274abeb9b3b0f38d4091573dd042a5d3b3aba
Fabio Francisco <fabinhofrancisco@gmail.com>
2020-11-20 23:48:37 -0300

    feat: cria estrutura da área de estatísticas.

---
+++ src/components/Statistic/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+function Statistic() {
+  return <Container />;
+}
+
+export default Statistic;
---
+++ src/components/Statistic/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  grid-area: ST;
+
+  display: flex;
+  height: 100%;
+  width: 100%;
+
+  background-color: green;
+`;
