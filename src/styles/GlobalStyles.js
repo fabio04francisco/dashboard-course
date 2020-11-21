fee7d83fcfe83d9b85adef88f9078fbf2aa7cc6e
Fabio Francisco <fabinhofrancisco@gmail.com>
2020-11-20 23:33:23 -0300

    feat: cria os estilos globais.

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body, #root{
+    width: 100%;
+  }
+
+  *, button, input{
+    border: 0;
+    outline: 0;
+    font-family: var(--font-primary);
+  }
+
+  :root {
+    --background-color: #FFFFFF;
+    --primary: #0C0B0B;
+    --secondary: #2f3136;
+    --iconMenu: #FFFFFF;
+    --opacity: 0.3;
+    --font-primary: 'Roboto';
+    --font-secondary: 'Baloo Bhaina 2';  
+  }
+`;
