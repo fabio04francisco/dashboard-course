b8ed3ed206ffc9f579ce3035507a47c04dc3708d
Fabio Francisco <fabinhofrancisco@gmail.com>
2020-11-30 22:24:00 -0300

    feat: criado navegação da seção estatisticas do usuário.

--- src/components/Statistic/index.js
+++ src/components/Statistic/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import {
+  Container,
+  NavigationStatistic,
+  SearchIcon,
+  BellIcon,
+  ArrowDownIcon,
+} from './styles';
 
-import { Container } from './styles';
+import Avatar from '../Avatar';
 
 function Statistic() {
-  return <Container />;
+  return (
+    <Container>
+      <NavigationStatistic>
+        <div>
+          <SearchIcon />
+          <input type="text" placeholder="Search" />
+        </div>
+
+        <div>
+          <BellIcon />
+          <Avatar />
+          <ArrowDownIcon />
+        </div>
+      </NavigationStatistic>
+    </Container>
+  );
 }
 
 export default Statistic;
--- src/components/Statistic/styles.js
+++ src/components/Statistic/styles.js
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
+import Bell from '../Icons/Bell';
+import Search from '../Icons/Search';
+import ArrowDown from '../Icons/ArrowDown';
+
 export const Container = styled.div`
   grid-area: ST;
 
@@ -7,5 +11,71 @@ export const Container = styled.div`
   height: 100%;
   width: 100%;
 
-  background-color: green;
+  padding: 0px 0px 0 36px;
+`;
+
+export const NavigationStatistic = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  height: 43px;
+  width: 439px;
+  padding: 3px 0 0 0;
+  margin-top: 52px;
+
+  & > div:nth-child(1) {
+    display: flex;
+    align-items: center;
+
+    height: 40px;
+    width: 300px;
+
+    border-radius: 8px;
+
+    background-color: var(--background-secundary);
+
+    > input {
+      background-color: inherit;
+    }
+  }
+
+  & > div:nth-child(2) {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    width: 100%;
+
+    > div {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 40px;
+      height: 40px;
+
+      margin: 0 8px 0 18px;
+
+      > img {
+        height: inherit;
+        width: auto;
+        object-fit: fill;
+        -webkit-filter: grayscale(100%);
+        filter: grayscale(100%);
+      }
+    }
+  }
+`;
+export const SearchIcon = styled(Search)`
+  height: 20px;
+  width: 20px;
+
+  margin: 0 10px 0 15px;
+`;
+export const BellIcon = styled(Bell)`
+  height: 23px;
+  width: 26px;
+`;
+export const ArrowDownIcon = styled(ArrowDown)`
+  height: 4px;
+  width: 7px;
 `;
