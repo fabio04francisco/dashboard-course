e707631ab36322491c91d21298f5c33d4adf52da
Fabio Francisco <fabinhofrancisco@gmail.com>
2020-11-30 22:19:45 -0300

    feat: adiconado novas imagens e icones.

---
+++ src/components/Avatar/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import imageAvatar from '../../assets/avatar.png';
+
+function Avatar() {
+  return (
+    <div>
+      <img src={imageAvatar} alt="Avatar" />
+    </div>
+  );
+}
+
+export default Avatar;
